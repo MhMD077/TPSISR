df76b56116e4f7fb5f161f3ea13312e6ea5ade95
MhMD077 <m-hemed.ben-mabrouk@efrei.net>
2026-03-04 09:53:57 +0100

    Update database connection IP address

--- connexion.php
+++ connexion.php
@@ -1,5 +1,5 @@
 <?php 
-$conn = new mysqli("10.3.208.122","root","tpsisr","koo_2_fourchette");
+$conn = new mysqli("192.168.252.138","root","tpsisr","koo_2_fourchette");
 if($conn->connect_error){
     die('erreur:'.$conn->connect_error);
 } else{
